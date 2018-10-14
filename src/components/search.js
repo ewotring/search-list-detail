1385799c26e73d6fec5fe0982f7561a83c479f5d
Erik Wotring <ewotring@gmail.com>
2018-10-14 13:12:24 -0500

    Filter data

--- src/components/search.js
+++ src/components/search.js
@@ -6,34 +6,66 @@ class Search extends Component {
     super(props);
     this.state = {
       items: [],
-      query: ''
+      query: '',
+      suggestions: []
     }
-    console.log(this.props.items)
+    // console.log(this.props.items)
   }
 
   componentDidMount() {
     this.setState({
-      items: this.props.items
+      items: this.props.items,
+      suggestions: this.props.items
     })
+    // console.log(this.state.items)
   }
 
   handleInputChange = () => {
     this.setState({
       query: this.search.value
     })
+    // this.getSuggestions(this.state.query)
   }
 
+  getSuggestions = event => {
+    event.preventDefault();
+    // console.log(this.state.suggestions)
+    console.log(this.state.query)
+    const inputValue = this.state.query.trim().toLowerCase()
+    this.setState({
+      suggestions: this.state.suggestions.filter(
+        item => {
+          return item.dba_name.toLowerCase().includes(inputValue)
+        }
+      )
+    })
+    // return this.state.suggestions.filter(
+    //   item => {
+    //     // console.log(item)
+    //     // console.log(item.dba_name)
+    //     // console.log(item.dba_name.toLowerCase())
+    //     return item.dba_name.toLowerCase().includes(inputValue)
+    //   }
+    // )
+  }
 
   render() {
+    console.log(this.state.query)
+    console.log(this.state.items)
+    console.log(this.state.suggestions)
     return (
       <div>
-        <form>
+        <form onSubmit={this.getSuggestions}>
           <input
             placeholder="Start typing a business name"
             ref={input => this.search = input}
             onChange={this.handleInputChange}
           />
           <p>{this.state.query}</p>
+          <input
+            type="submit"
+            value="Submit"
+          />
         </form>
         <List items={this.state.items} />
       </div>
